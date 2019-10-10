9584217d516525aaa2b6142eada7c2be33894444
jakubmac <jakub.faldasz@icloud.com>
2019-10-10 20:46:39 +0200

    Change spacing of elements, change the background, add some libraries

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,15 +11,34 @@ $(window).on("orientationchange load resize", function () {
     if (width <= 768) {
         $(".collapse").removeClass("showMenu");
     }
-})
+});
 
 $(document).scroll(() => {
     var nav = $("#navbar");
-    var aboutWasntSeen = true;
     nav.toggleClass("navbarOnScroll", $(this).scrollTop() > nav.height());
-    if(aboutWasntSeen) {
-        $(".aboutContent img").fadeIn(1000);
-        $(".description").fadeIn(1000);
-    }
 });
 
+new Waypoint({
+    element: document.querySelector("#about"),
+    handler: function() {
+        var img = $("#faceImage img");
+        var desc = $(".description");
+        
+        img.animate({left: '0'}, 1000); 
+        desc.animate({left: '0'}, 1000);
+        console.log("bounce");
+    },
+    offset: 700
+});
+
+new Waypoint({
+    element: document.querySelector("#skills"),
+    handler: function () {
+        var level;
+        $(".progressBar span").each(function () {
+            level = $(this).text();
+            $(this).css("width", level);
+        });
+    },
+    offset: 800
+});
